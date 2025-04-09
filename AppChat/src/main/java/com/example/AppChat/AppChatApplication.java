9f4c9e94b7a21a4dccb129e976f24ed2f3e63ef5
yasser rachih <yassirach17@gmail.com>
2025-04-09 15:39:06 +0100

    files

--- AppChat/src/main/java/com/example/AppChat/AppChatApplication.java
+++ AppChat/src/main/java/com/example/AppChat/AppChatApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class AppChatApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AppChatApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AppChatApplication.class, args);
+    }
 }
--- AppChat/src/main/java/com/example/AppChat/controller/authController.java
+++ AppChat/src/main/java/com/example/AppChat/controller/authController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 
-public class authController {
-@GetMapping("/auth")
-public String showAuthPagr() {
+public class AuthController {
+@GetMapping("/")
+public String showAuthPage() {
     return "auth";
 }
 
